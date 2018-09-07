e7a38d2320508261da6e5e7be4d5335e3e79e800
pravallikachinky <41135712+pravallikachinky@users.noreply.github.com>
2018-09-07 17:39:45 +0530

    V1

---
+++ Lab/src/com/cg/beans/CompTrak.java
@@ -0,0 +1,17 @@
+package com.cg.beans;
+
+
+
+public class CompTrak {
+
+	private String computerTrack;
+	private String diskCapacity;
+	private String totalInsMem;
+	private String NetCardNum;
+	private String NetCardManufact;
+	private String freeSpace;
+	private String operatingSystem;
+	private String osVersion;
+	
+	
+}
